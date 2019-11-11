7cc303141135315c789ee70f1c8844d46f4fb0d2
shenjiayin <57580046+shenjiayin@users.noreply.github.com>
2019-11-11 23:07:23 +0800

    Create 1903050108shenjiayin.cpp
    前两次作业
    1903050108 沈佳音

---
+++ 1903050108shenjiayin.cpp
@@ -0,0 +1,147 @@
+the first homework
+<!DOCTYPE HTML>
+<html>
+
+<body>
+    <h1 style="text-align:center;color:yellow"><a href="#">到底部指定位置</a></h1>
+    <hr/>
+    <h2 style="text-align:center;color:aqua">世间美好与你环环相扣</h2>
+    <pre><p style="font-size:15px">
+                                                                            偏偏秉烛夜 游
+                                                                            午夜星辰 似奔走之友
+                                                                            爱你每个结痂伤口
+                                                                             酿成的陈年烈酒
+                                                                            入喉尚算可口
+                                                                            怎么泪水 还偶尔失守
+                                                                            邀你细看心中缺口
+                                                                            裂缝中留存 温柔
+                                                                            此时已莺飞草长 爱的人正在路上
+                                                                            我知他风雨兼程 途经日暮不赏
+                                                                            穿越人海 只为与你相拥
+                                                                            此刻已皓月当空 爱的人手捧星光
+                                                                            我知他乘风破浪 去了黑暗一趟
+                                                                            感同身受 给你救赎热 望
+                                                                             <img src="http://pic143.nipic.com/file/20171013/3046502_144350141000_2.jpg"width="300px" height="300px">
+                                                                             知道你不能 还要你感受
+                                                                             让星光加了一点彩虹
+                                                                             让樱花偷偷 吻你额头
+                                                                             让世间美好 与你环环相扣
+                                                                             此时已莺飞草长 爱的人正在路上
+                                                                             我知他风雨兼程 途经日暮不赏
+                                                                             穿越人海 只为与你相拥
+                                                                             此刻已皓月当空 爱的人手捧星光
+                                                                             我知他乘风破浪 去了黑暗一趟
+                                                                             感同身受 给你救赎热 望
+                                                                             此时已莺飞草长 爱的人正在路上
+                                                                             我知他风雨兼程 途经日暮不赏
+                                                                             穿越人海 只为与你相拥
+                                                                             此刻已皓月当空 爱的人手捧星光
+                                                                             我知他乘风破浪 去了黑暗一趟
+                                                                             感同身受 给你救赎热 望
+                                                                             知道你不能 还要你感受
+                                                                             让星光加了一点彩虹
+                                                                             当樱花开的纷纷扬扬
+                                                                             当世间美好 与你环环相扣  </p></pre> </p>
+    </pre>
+    <hr/>
+    <hr/>
+    <h2 style="text-align:center;color:coral">详细情况:<a href="http://pc.baojia056.cn/soft/400602.html" ;style="color:green">QQ音乐</a></h2>
+    <hr/>
+    <hr/>
+    <hr/>
+    <h1 style="text-align:center"><a href="" style="color:magenta">回到顶部 联系我们</a></h1>
+    the second homework
+    <! DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>国庆阅兵</title>
+    <style tape="text/css">
+        .p1 {
+            margin: 20px;
+            padding: 20px;
+        }
+        
+        body {
+            background-image: url(http://special.71.cn/190719/index/resource/shan.jpg)
+        }
+        
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        
+        .nav {
+            list-style: none;
+            background-color: #A9A9A9;
+            width: 2000px;
+            height: 30px;
+            overflow: hidden;
+            position: fixed;
+        }
+        
+        a:hover,
+        a :active {
+            text-decoration: underline;
+            background-color: red;
+        }
+        
+        .nav li {
+            float: left;
+        }
+        
+        .nav a {
+            display: block;
+            text-decoration: none;
+            color: black;
+            text-align: center;
+            padding: 10px;
+            width: 350px;
+        }
+        
+        .nv {
+            height: 40px;
+            width: 2000px;
+            background-color: rgb(0, 162, 255);
+            margin-bottom: 0px;
+            margin-top: 20px
+        }
+    </style>
+</head>
+
+<body>
+    <ul class="nav">
+        <li><a href=: "#">国庆阅兵</a>
+            <li><a href=: "#">献上祝福</a>
+                <li><a href=: "#">评论</a>
+                    <li><a href=: "#">网友之家</a>
+                        <li><a href=: "#">网友祝福</a>
+                        </li>
+
+    </ul>
+    <h1 style="color:red;text-align:center;"><b>国庆阅兵</b></h1>
+    <p1><img style="width:800px;height:500px;margin-top:100px;margin-left:100px;float:left" src="http://i3.sinaimg.cn/cj/hy/20091224/U2104P31T1D7150865F46DT20091224161555.jpg">
+        <p2 style="float:right;margin-top:100px;margin-right:400px;"><a style="color:black;text-decoration: none;" href="http://www.sohu.com/a/345895766_267807"><b><big>"书影中的70年，新中国图书版本展 "在京开幕</big></b></a> </p2>
+        <p2 style="float:right;margin-top:100px;margin-right:300px;"><a style="color:black;text-decoration: none;" href="http://www.chinanews.com/gn/2019/10-06/8972480.shtml"><b><big>我和我的国旗——香港青少年与内地青联委员开展交流活动</big></b></a> </p2>
+        <p2 style="float:right;margin-top:100px;margin-right:550px;"><a style="color:black;text-decoration: none;" href="http://www.npc.gov.cn/npc/c30834/201910/d86782191a6043f5a008c74c043c9ace.shtml"><b><big>同贺伟大成就　共创美好未来</big></b></a> </p2>
+        <p2 style="float:right;margin-top:100px;margin-right:450px;"> <a style="color:black;text-decoration: none;" href="https://news.china.com/domestic/945/20191002/37145460.html"><b><big>香港举行多项活动庆祝新中国成立70周年</big></b> </a></p2>
+        <h1 style="color:red;text-align:center;margin-top:700px;"><b>为祖国献上祝福</b></h1>
+        <form action="" style="text-align:center">
+            <big>祝福<input type="text" name="usename" style="margin-top:20px;" /></big><br/> <big>姓名
+            <input type="text" name="usename" style="margin-top:10px;" /></big><br/>
+            <input type="submit" value="献上祝福" style="background-color:red;width:200px;height:40px" /> </form>
+    </p1>
+    <div class="nv">
+        <p3 style="margin-left:800px;color:white">Copyright@沈阳理工大学|联系我们</p3><br/>
+        <p4 style="margin-left:830px;color:white;margin-top:5px">1903050108 沈佳音制作</p4>
+
+
+    </div>
+
+
+
+
+</body>
+
+</html>
