4498ce6ed7f4676e466129bda57453502ee0ec8e
luying0514 <57579980+luying0514@users.noreply.github.com>
2019-11-14 23:14:07 +0800

    Create myprogram.cpp

---
+++ 1903050207/myprogram.cpp
@@ -0,0 +1,147 @@
+第一次作业
+<! DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>李易峰《动物世界》</title>
+<style type="text/css">
+body{font-size:15px}
+h2{color:blue}
+p1{color:purple}
+p2{color:green}
+p6{color:pink}
+p7{color:yellow}
+a:hover{color:red}
+</style>
+</head>
+<center>
+<body>
+<h1 id="dingbu">动物世界-郑开司</h1>
+<h2>李易峰</h2>
+<p1>〔1〕该做的事我已经做了 ，该走的路已经走到了尽头 ，我守住了我信的道。 生活将会继续下去， 我的脑子依然有病，我仍然是他们眼中的小丑。<hr /></p>
+ <p2>〔2〕该打的仗我已经打过了，该跑的路我也跑到了尽头老子信的道老子自己来守。<hr/></P>
+<p3>背叛、争抢 、没有底线 ，想把老子变成一只动物。no 没戏 。<hr /></p>
+ <p4> 老子宁可做一辈子披荆斩刺的小丑 ，也绝不会变成你们这种人渣的样子。<hr/></p>
+ <p5> 游戏是你们的，规则老子自己来定。<hr /></p>
+  <p6> 〔3〕我不会成为一个软弱的好人，但也永远不会沦为一个凶残的坏人。 别问我为什么，我脑子有病。<hr /></p>
+  <p7> 〔4〕想像他一样做一个勇敢的人，在现实世界里披荆斩棘。 做自己该做的，坚持自己想坚持的。守住自己的道。<hr/>   </p>
+<img src="D:\34\李易峰.jpg" alt="李易峰" width="300" height="300">
+
+
+<a href="#dingbu">跳转到顶部</a>
+
+<a href ="mailto:2509475560@qq.com">联系我们</a>
+</body>
+</center>
+</html>
+
+
+第二次作业
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>庆祝祖国成立七十周年</title>
+    <style type="text/css">
+        #footer {
+            height: 70px;
+            line-height: 40px;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+            text-align: center;
+            background: rgb(46, 105, 233);
+            color: #fff;
+            font-family: Arial;
+            font-size: 12px;
+            letter-spacing: 1px;
+        }
+        
+        h1 {
+            text-align: center;
+            color: red;
+        }
+        
+        #nav ul {
+            Width: 1000px;
+            height: 40px;
+            margin: 40px auto;
+            padding: 0;
+            list-style: none;
+            border-top: solid 5px #FFFFFF;
+            border-bottom: solid 5px #FFFFFF;
+            background: url(D:\34\1.jpg);
+        }
+        
+        #nav ul li {
+            width: 100px;
+            float: right;
+            text-align: center;
+            font: 16px/2.5"microsoft yahei";
+        }
+        
+        #nav ui li a {
+            color: #800080;
+            text-decoration: none;
+        }
+        
+        #nav ui li a:hover {
+            display: block;
+            color: #FFFFFF;
+            background: #DC143C;
+        }
+        
+        div {
+            width: 1250px;
+            height: 600px;
+            background: url(http://special.71.cn/190719/index/resource/shan.jpg);
+        }
+        
+        a:hover {
+            color: crimson;
+        }
+        
+        img {
+            float: left
+        }
+    </style>
+</head>
+
+<body>
+    <div id="nav">
+        <ul>
+            <li><a href="#" />国庆阅兵</a>
+            </li>
+            <li><a href="#" />献上祝福</a>
+            </li>
+            <li><a href="#" />评论</a>
+            </li>
+            <li><a href="#" />网友之家</a>
+            </li>
+            <li><a href="#" />网友祝福</a>
+            </li>
+        </ul>
+        <h1>国庆阅兵</h1>
+
+        <img src="D:\34\阅兵.jpg" alt="阅兵" style="float :right" width="200" height="200"><br/><br/><br/><br/>
+        <a href="mailto:2509475560@qq.com">“书影中的70年-新中国书版本展”在京开幕 <br/></a>
+        <a href="mailto:2509475560@qq.com ">我和我的国旗——香港青少年与内地青联委员开展交流活动 <br/></a>
+        <a href="mailto:2509475560@qq.com ">同贺伟大成就 共创美好未来<br/></a>
+        <a href="mailto:2509475560@qq.com ">香港举行多项活动庆祝新中国成立70周年 <br/><br/><br/></a>
+        <center>
+            <h1>为祖国献上祝福</h1>
+            <input name="key" type="text" id="key" value="祝福" size="30" onmouseover=this.focus();this.select(); onclick="
+        if(value==defaultValue){value='';this.style.color='#000'}" onBlur="if(!value){value=defaultValue;this.style.color='#999'}" style="color:#999" /><br/>
+            <input name="key" type="text" id="key" value="姓名" size="30" onmouseover=this.focus();this.select(); onclick="if(value==defaultValue){value='';this.style.color='#000'}" onBlur="if(!value){value=defaultValue;this.style.color='#999'}" style="color:#999"
+            /><br/>
+            <button type="献上祝福" value="献上祝福">献上祝福</button>
+        </center>
+        <div id="footer">CopyRight@沈阳理工大学||联系我们<br/>15774869618 路颖 制作 </div>
+    </div>
+
+
+
+</body>
+
+</html>
